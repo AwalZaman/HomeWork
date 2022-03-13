9964f67f018d355ab128dc59ea145e894788acf2
AwalZaman <awalzamanqa@gmail.com>
2022-03-13 08:59:40 -0400

    hw11

--- src/hw11Q2Polymorphism/TestFamily.java
+++ src/hw11Q2Polymorphism/TestFamily.java
@@ -6,10 +6,8 @@ public class TestFamily {
 		System.out.println("\n------------------- Sister ---------------------\n");
 		Sister sis = new Sister();
 		sis.sister();
-		sis.sister(7);
 		sis.sister(3, 7);
 		sis.sister(4, 5, 6);
-		sis.sister("2", 9);
 		Sister.sister(8, "7"); //static method called by sister Class itself
 		sis.sister(1, 2, 5, 9);
 		sis.sister("4");
@@ -17,10 +15,8 @@ public class TestFamily {
 		System.out.println("\n------------------- Niece ---------------------\n");
 		Niece nc = new Niece();
 		nc.sister();
-		nc.sister(7);
 		nc.sister(3, 7);
 		nc.sister(4, 5, 6);
-		nc.sister("2", 9);
 		// static method can't be overriden in child class Niece
 		//final method can't be overriden in child class Niece
 		nc.sister("4");
